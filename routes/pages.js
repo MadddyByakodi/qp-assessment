d7ce43be06353fee491a079d352dd0077f1869f9
Maddy <maddy.byakodi@gmail.com>
2024-02-02 09:23:01 +0530

    orderchanges

--- routes/pages.js
+++ routes/pages.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/register",(req, res)=>{
-    res.render('register');
-});
-router.get("/login",(req, res)=>{
-    res.render('login');
-});
-
 router.get("/",(req, res)=>{
     res.render('index');
 });
